7e769cd00e75de94285413b913def5263ffec77e
Bayanda Jack <bayanda45jack@gmail.com>
2025-03-03 21:02:46 +0200

    Added some js code

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = function() {
     window.scrollTo(0, 0);
 };
+
+let button = document.getElementById(formButton);
+button.onclick = function(){
+    let username = document.getElementById("userName").value;
+    let email = document.getElementById("userEmail").value;
+    let issue = document.getElementById("legalproblem").value;
+}
+
+console.log(`My name is ${username} and my email is ${email}. My legal issue is here: ${issue}`);
