e60b2ec2b34dd5fd09ffa4f49b324962c5298278
Bayanda Jack <bayanda45jack@gmail.com>
2025-03-15 08:25:21 +0200

    Added a php file for mailing

---
+++ send_email.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST["legalproblem"]);
+
+    // Law firm's email
+    $to = "bayanda45jack@gmail.com";  // Replace with actual law firm email
+    $subject = "New Contact Request from $name";
+    
+    // Email Body
+    $body = "You have received a new message from:\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+    
+    // Headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send Email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
